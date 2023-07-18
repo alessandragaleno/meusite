285c151d610c3eeeb4d88874f4c4d1eee5b6033a
alessandragaleno <alessandragaleno421@gmail.com>
2023-07-17 21:30:06 -0300

    avisojs

---
+++ assets/js/hora.js
@@ -0,0 +1,17 @@
+setInterval(function data()(
+    contst local hora = document.querySelector('#hora')
+    contst hoje = new Date();
+    contst hora = hoje.getHours();
+    contst minuto = hoje.getMinuto();
+    contst segundo = hoje.getSegundos();
+    contst horaAtual = $(hora) :${minuto} :${segundos}
+    if(hora.parseInt)
+    local hora.textContent 
+),1000)
+
+const resultado = document,getElementById("resultado")
+function nome usuario(){
+    var nome = document.getElementById("nome").value:
+
+    resultado.
+}
